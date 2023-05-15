cd6fc7664020f8006d050c88712aa8114ca97c23
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-05-15 19:18:32 +0800

    Add data manager for submissions

--- GlassRoomAPI/Sources/GlassRoomAPI/DataTypes/courses.courseWork.studentSubmissions/StudentSubmission.swift
+++ GlassRoomAPI/Sources/GlassRoomAPI/DataTypes/courses.courseWork.studentSubmissions/StudentSubmission.swift
@@ -26,3 +26,19 @@ public struct StudentSubmission: Codable {
     public var shortAnswerSubmission: ShortAnswerSubmission
     public var multipleChoiceSubmission: MultipleChoiceSubmission
 }
+
+public extension StudentSubmission {
+    var creationDate: Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+
+        if let date = dateFormatter.date(from: creationTime) {
+            return date
+        } else {
+            print("Could not get date for: \(creationTime)")
+            return Date()
+        }
+    }
+}
---
+++ The GlassRoom/Model/DataManagers/CourseWorkSubmissionDataManager.swift
@@ -0,0 +1,100 @@
+//
+//  CourseWorkSubmissionDataManager.swift
+//  The GlassRoom
+//
+//  Created by Kai Quan Tay on 15/5/23.
+//
+
+import Foundation
+import GlassRoomAPI
+
+class CourseWorkSubmissionDataManager: ObservableObject {
+    @Published private(set) var submissions: [StudentSubmission] = []
+    @Published private(set) var loading: Bool = false
+    @Published private(set) var nextPageToken: String?
+
+    let courseId: String
+    let courseWorkId: String
+
+    init(courseId: String, courseWorkId: String) {
+        self.courseId = courseId
+        self.courseWorkId = courseWorkId
+
+        if var existingManagers = CourseWorkSubmissionDataManager.loadedManagers[courseId] {
+            existingManagers[courseWorkId] = self
+            CourseWorkSubmissionDataManager.loadedManagers[courseId] = existingManagers
+        } else {
+            CourseWorkSubmissionDataManager.loadedManagers[courseId] = [courseWorkId: self]
+        }
+    }
+
+    func refreshList(requestNextPageIfExists: Bool = false) {
+        loading = true
+    }
+
+    func clearCache(courseId: String) {
+        FileSystem.write([StudentSubmission](), to: "\(courseId)_\(courseWorkId)_submissions.json")
+    }
+
+    func readCache() -> [StudentSubmission] {
+        // if the file exists in CourseCache
+        if FileSystem.exists(file: "\(courseId)_\(courseWorkId)_submissions.json"),
+           let cacheItems = FileSystem.read([StudentSubmission].self, from: "\(courseId)_\(courseWorkId)_submissions.json") {
+            return cacheItems
+        }
+        return []
+    }
+
+    func writeCache() {
+        FileSystem.write(submissions, to: "\(courseId)_\(courseWorkId)_submissions.json") { error in
+            print("Error writing: \(error.localizedDescription)")
+        }
+    }
+
+    func refreshList(nextPageToken: String? = nil, requestNextPageIfExists: Bool = false) {
+        GlassRoomAPI.GRCourses.GRCourseWork.GRStudentSubmissions.list(
+            params: .init(courseId: courseId, courseWorkId: courseWorkId),
+            query: .init(userId: nil,
+                         states: nil,
+                         late: nil,
+                         pageSize: nil,
+                         pageToken: nextPageToken),
+            data: .init()
+        ) { response in
+            switch response {
+            case .success(let success):
+                DispatchQueue.main.async {
+                    self.submissions.mergeWith(other: success.studentSubmissions,
+                                                       isSame: { $0.id == $1.id },
+                                                       isBefore: { $0.creationDate > $1.creationDate })
+                }
+                if let token = success.nextPageToken, !token.isEmpty, requestNextPageIfExists {
+                    self.refreshList(nextPageToken: token,
+                                     requestNextPageIfExists: requestNextPageIfExists)
+                } else {
+                    DispatchQueue.main.async {
+                        self.nextPageToken = success.nextPageToken
+                        self.loading = false
+                        self.writeCache()
+                    }
+                }
+            case .failure(let failure):
+                print("Failure: \(failure.localizedDescription)")
+                DispatchQueue.main.async {
+                    self.loading = false
+                }
+            }
+        }
+    }
+
+    // MARK: Static functions
+    static private(set) var loadedManagers: [String: [String: CourseWorkSubmissionDataManager]] = [:]
+
+    static func getManager(for courseId: String, courseWorkId: String) -> CourseWorkSubmissionDataManager {
+        if let courseManagers = loadedManagers[courseId],
+           let manager = courseManagers[courseWorkId] {
+            return manager
+        }
+        return .init(courseId: courseId, courseWorkId: courseWorkId)
+    }
+}
