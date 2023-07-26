f51fc9f963aa55993dae175bb13ae1c693bb52fc
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-07-26 10:18:41 +0800

    Remove sensitive scopes

--- Shared/Model/UserAuthModel.swift
+++ Shared/Model/UserAuthModel.swift
@@ -28,15 +28,15 @@ class UserAuthModel: ObservableObject {
     }
 
     let neededScopes: [String] = [
-        "https://www.googleapis.com/auth/classroom.courses",
+        "https://www.googleapis.com/auth/classroom.courses.readonly",
         "https://www.googleapis.com/auth/classroom.announcements",
         "https://www.googleapis.com/auth/classroom.coursework.students",
         "https://www.googleapis.com/auth/classroom.coursework.me",
         "https://www.googleapis.com/auth/classroom.courseworkmaterials",
-        "https://www.googleapis.com/auth/classroom.rosters",
+        "https://www.googleapis.com/auth/classroom.rosters.readonly",
         "https://www.googleapis.com/auth/classroom.topics",
-        "https://www.googleapis.com/auth/classroom.profile.emails",
-        "https://www.googleapis.com/auth/classroom.profile.photos",
+//        "https://www.googleapis.com/auth/classroom.profile.emails",
+//        "https://www.googleapis.com/auth/classroom.profile.photos",
         "https://www.googleapis.com/auth/drive.readonly"
     ]
 
