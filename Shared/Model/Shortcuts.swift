d75d93a1bee4de7a4b5fcfba72a331b0da20bf9c
Tristan <tristanchay123@gmail.com>
2023-08-23 10:48:44 +0800

    added keyboard shortcut customisation and changed macOS target name to Glassroom

---
+++ Shared/Model/Shortcuts.swift
@@ -0,0 +1,8 @@
+//
+//  Shortcuts.swift
+//  The GlassRoom
+//
+//  Created by Tristan Chay on 23/8/23.
+//
+
+import Foundation
---
+++ Shared/Model/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  The GlassRoom
+//
+//  Created by Tristan Chay on 23/8/23.
+//
+
+import Foundation
