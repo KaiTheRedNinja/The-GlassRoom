98d0e6bb1de7d59b39b3550146977029e98e1bd7
Tristan <tristanchay123@gmail.com>
2024-01-16 12:07:24 +0800

    added visionos target

---
+++ Glassroom visionOS/Glassroom_visionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glassroom_visionOSApp.swift
+//  Glassroom visionOS
+//
+//  Created by Tristan Chay on 16/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Glassroom_visionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
