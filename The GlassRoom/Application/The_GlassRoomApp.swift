4f04de2ff10b13499d95edaaafb8b385311f1f4d
Tristan <tristanchay123@gmail.com>
2024-01-26 08:46:00 +0800

    glassroom now remembers last opened window size when reopening a window (not adding a new one tho)

--- The GlassRoom/Application/The_GlassRoomApp.swift
+++ The GlassRoom/Application/The_GlassRoomApp.swift
@@ -7,6 +7,23 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        let mainWindow = NSApp.windows.first
+        mainWindow?.delegate = self
+    }
+
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        let mainWindow = NSApp.windows.first
+        if flag {
+            mainWindow?.orderFront(nil)
+        } else {
+            mainWindow?.makeKeyAndOrderFront(nil)
+        }
+        return true
+    }
+}
+
 @main
 struct The_GlassRoomApp: App {
     
